f2707675937f855550e77dea34ca34ff4feafe06
Alexey <skif@skif-web.ru>
2019-07-06 22:55:37 +0300

    build as firmware
    1) beaglebone/sdcard.sh;beaglebone/genimage.cfg - use own genimage config;
    2) beaglebone/inside-fakeroot.sh: move systemd-netwowkd.conf; make second microsd partition with data
    3) Uboot - boot without sdcard/rootfs
    4) beaglebone - add openssl,bash, php, mysqld,lighthttpd;

---
+++ overlay/board/beaglebone/inside-fakeroot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#########################################################
+#
+# network settings
+#
+#########################################################
+
+# disable systemd-networkd && systemd-resolved
+# TODO
+
+# rename default config 
+mv ${TARGET_DIR}/etc/systemd/network/dhcp.network ${TARGET_DIR/}/etc/systemd/network/beaglebone.network
+
+#########################################################
+#
+# make data.ing for second sdcard partition
+#
+#########################################################
+dataImageFile="${BINARIES_DIR}/data.img"
+dataImageMountDir="${BINARIES_DIR}/data"
+dataImageFsType="ext4"
+dataImageFsLabel="monitorData"
+
+while mount|grep $dataImageMountDir > /dev/null
+do
+    sudo umount -l $dataImageFile
+done
+
+if [ -f $dataImageFile ]; then
+    rm -rf $dataImageFile
+fi
+
+if [ -d $dataImageMountDir ]; then
+    rm -rf $dataImageMountDir
+fi
+set -x
+dd if=/dev/zero of=$dataImageFile bs=16M count=1
+mkfs.$dataImageFsType -L $dataImageFsLabel $dataImageFile
+mkdir -p $dataImageMountDir
+sudo mount -t $dataImageFsType -o loop $dataImageFile $dataImageMountDir
+sudo cp ${TARGET_DIR/}/etc/systemd/network/beaglebone.network  $dataImageMountDir/
+sudo umount $dataImageMountDir
+
+mkdir ${TARGET_DIR}/data/
+
+#########################################################
+#
+# make fstab
+#
+#########################################################
+fstabFile="${TARGET_DIR}/etc/fstab"
+echo "" > $fstabFile
+
+echo "LABEL=$dataImageFsLabel /data $dataImageFsType  defaults   1 2" >> $fstabFile
