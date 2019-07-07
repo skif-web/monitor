c967b93765ef4def76ae99211965301dec8ee7ac
Alexey <skif@skif-web.ru>
2019-07-07 21:46:37 +0300

    New fix and features
    prepare.sh - update php settings; create database for zabbix until created
    prepare.sercice - now wanted by mysql server
    insidefakeroot - create zabbix-frontend config; index.php as default page

--- overlay/board/beaglebone/inside-fakeroot.sh
+++ overlay/board/beaglebone/inside-fakeroot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 #########################################################
 #
@@ -6,20 +7,12 @@
 #
 #########################################################
 
-# disable systemd-networkd && systemd-resolved
-# TODO
-
 # rename default config 
-mv ${TARGET_DIR}/etc/systemd/network/dhcp.network ${TARGET_DIR/}/etc/systemd/network/beaglebone.network
+if [ -f  ${TARGET_DIR}/etc/systemd/network/dhcp.network ]; then
+    mv ${TARGET_DIR}/etc/systemd/network/dhcp.network ${TARGET_DIR/}/etc/systemd/network/beaglebone.network
+fi
 
-#########################################################
-#
-# make fstab
-#
-#########################################################
-fstabFile="${TARGET_DIR}/etc/fstab"
-echo "" > $fstabFile
-echo "LABEL=$dataImageFsLabel /data $dataImageFsType  defaults   0 1" >> $fstabFile
+
 
 #########################################################
 #
@@ -39,11 +32,10 @@ done
 
 #########################################################
 #
-# add apache php support
+# apache
 #
 #########################################################
-
-set -x
+# Add php
 grep -q 'SetHandler application/x-httpd-php' ${TARGET_DIR}/etc/apache2/httpd.conf
 php_apache_status=$?
 echo @$php_apache_status@
@@ -54,6 +46,12 @@ if [ $php_apache_status -gt 0 ]; then
 </FilesMatch>
 EOF
 fi
+# Set index.php as default
+rm ${TARGET_DIR}/usr/htdocs/index.html
+
+sed -i -e 's/DirectoryIndex index.html/DirectoryIndex index.php/g' ${TARGET_DIR}/etc/apache2/httpd.conf
+
+
 
 
 #########################################################
@@ -65,6 +63,42 @@ fi
 # Set dbPassword for zabbix server
 zabbix_password_status=grep -q 'DBPassword=zabbix' ${TARGET_DIR}/etc/zabbix_server.conf || \
     sed -i -e '/DBPassword=/a\' -e 'DBPassword=zabbix' ${TARGET_DIR}/etc/zabbix_server.conf
+
+#########################################################
+#
+# move  zabbix frontend to html  dir
+#
+#########################################################
+
+if [ -d ${TARGET_DIR}/usr/zabbix/php-frontend ]; then
+    mv ${TARGET_DIR}/usr/zabbix/php-frontend/* ${TARGET_DIR}/usr/htdocs/
+    rm -rf ${TARGET_DIR}/usr/zabbix/php-frontend
+fi
+
+cat > ${TARGET_DIR}/usr/htdocs/conf/zabbix.conf.php <<EOF
+<?php
+// Zabbix GUI configuration file.
+global $DB;
+
+\$DB['TYPE']     = 'MYSQL';
+\$DB['SERVER']   = 'localhost';
+\$DB['PORT']     = '0';
+\$DB['DATABASE'] = 'zabbix';
+\$DB['USER']     = 'zabbix';
+\$DB['PASSWORD'] = 'zabbix';
+
+// Schema name. Used for IBM DB2 and PostgreSQL.
+\$DB['SCHEMA'] = '';
+
+\$ZBX_SERVER      = 'localhost';
+\$ZBX_SERVER_PORT = '10051';
+\$ZBX_SERVER_NAME = '';
+
+\$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+EOF
+
+
+
 
 #########################################################
 #
@@ -88,14 +122,20 @@ fi
 if [ -d $dataImageMountDir ]; then
     rm -rf $dataImageMountDir
 fi
-set -x
 dd if=/dev/zero of=$dataImageFile bs=16M count=1
 mkfs.$dataImageFsType -L $dataImageFsLabel $dataImageFile
 mkdir -p $dataImageMountDir
 sudo mount -t $dataImageFsType -o loop $dataImageFile $dataImageMountDir
 sudo cp ${TARGET_DIR/}/etc/systemd/network/beaglebone.network  $dataImageMountDir/
 sudo cp ${TARGET_DIR}/etc/zabbix_server.conf $dataImageMountDir/
-sudo cp ${TARGET_DIR}/etc/zabbix_agent.conf $dataImageMountDir/
+sudo cp ${TARGET_DIR}/etc/zabbix_agentd.conf $dataImageMountDir/
 sudo umount $dataImageMountDir
 
-mkdir ${TARGET_DIR}/data/
+#########################################################
+#
+# make fstab
+#
+#########################################################
+fstabFile="${TARGET_DIR}/etc/fstab"
+echo "" > $fstabFile
+echo "LABEL=$dataImageFsLabel /data $dataImageFsType  defaults   0 1" >> $fstabFile
--- overlay/board/beaglebone/systemd/prepare.sh
+++ overlay/board/beaglebone/systemd/prepare.sh
@@ -25,8 +25,10 @@ if [ -f /data/zabbix_server.conf ]; then
     cp /data/zabbix_server.conf /etc/zabbix_server.conf
 fi
 
-mv /usr/zabbix/php-frontend/* ${TARGET_DIR}/var/www/
+if [ -f /data/zabbix.conf.php ]; then
+    cp /data/zabbix.conf.php /usr/htdocs/conf/zabbix.conf.php
 
+fi
 
 #########################################################
 #
@@ -36,11 +38,14 @@ mv /usr/zabbix/php-frontend/* ${TARGET_DIR}/var/www/
 
 # permissions
 # mysql -u root --execute "create database zabbix character set utf8 collate utf8_bin;"
-mysql -u root --execute "create database zabbix;"
-mysql -u root --execute "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"
-
+while true
+do
+    mysql -u root --execute "create database zabbix;" && \
+    mysql -u root --execute "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';" && \
+    break
+    sleep 0.5
+done
 # Try to find any dump
-set -x
 sqlDump=`ls -lt /data/zabbix_dump_*.sql 2>/dev/null|head -n1|awk '{print $NF}'`
 
 if [ "x$sqlDump" != "x" ]; then
@@ -54,3 +59,13 @@ else
 fi
 # DEBUG
 date > /date.txt
+
+#########################################################
+#
+# php settings fix
+#
+#########################################################
+
+sed -i -e 's/post_max_size = 8M/post_max_size = 16M/g' /etc/php.ini
+sed -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php.ini
+sed -i -e 's/max_input_time = 60/max_input_time = 300/g' /etc/php.ini
