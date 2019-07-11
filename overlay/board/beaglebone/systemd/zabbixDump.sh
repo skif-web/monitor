52225eb92007a37f71533f73c2681bf84d06c2ca
Alexey Lukyanchuk <skif@skif-web.ru>
2019-07-11 14:50:20 +0300

    add services for mysql dump(every 15 min and at shutdown)
    Signed-off-by: Alexey Lukyanchuk <skif@skif-web.ru>

---
+++ overlay/board/beaglebone/systemd/zabbixDump.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+
+DATADIR=/data
+# Get current size of zabbix DB
+size=`/bin/mysql -uroot -e "select table_schema,ROUND(sum(data_length + index_length) / 1024 / 1024) \
+ from information_schema.tables WHERE table_schema = 'zabbix' GROUP BY table_schema;"|grep zabbix|awk '{print $NF}'`
+echo $size
+
+# First, check free space on /data
+while true; do
+    free_space=`df -k|grep '/data'|awk '{print $4}'`
+    free_space=`expr $free_space / 1024`
+    if [ $free_space -gt $size ];then
+        break
+    fi
+done
+
+# Second, remove dump's exept last 10
+find /data -iname zabbix_dump_\*.sql|sort -r| tail -n +10|xargs rm -rf
+
+# Last, do dump
+date_stamp=`date +"%s"`
+/bin/mysqldump zabbix >/data/zabbix_dump_$date_stamp.sql 
