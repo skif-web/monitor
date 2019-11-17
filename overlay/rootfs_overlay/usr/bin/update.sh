8679b25c39c9b130f5a6d3a580a877495c9128cd
Alexey <skif@skif-web.ru>
2019-11-17 13:22:32 +0300

    self-update script. Also saved in update package

---
+++ overlay/rootfs_overlay/usr/bin/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+src="/tmp/update"
+
+update_dir=`mktemp -d`
+mkdir -p $update_dir
+mount LABEL=system  $update_dir
+
+cd  $src/
+sha256sum -c checksum.txt && \
+cp $src/bzImage $update_dir/ && \
+sync && \
+umount $update_dir
