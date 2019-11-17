22e60af04a6f5e6e4502a98f36b17289fef3fd7f
Alexey <skif@skif-web.ru>
2019-11-17 13:29:36 +0300

    post-build: create update archive

---
+++ overlay/post-build/0099-update-arch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+update_dir="${BINARIES_DIR}/update"
+
+name=`grep BR2_TARGET_GENERIC_HOSTNAME .config|awk -F= '{print $2}'|tr -d \"`
+
+
+rm -rf ${BINARIES_DIR}/update
+rm -rf ${BINARIES_DIR}/${name}.tar.gz
+
+mkdir -p ${BINARIES_DIR}/update
+
+cp ${BINARIES_DIR}/bzImage ${update_dir}
+cp ${TARGET_DIR}/usr/bin/update.sh ${update_dir}/
+cd ${update_dir}/
+sha256sum * > checksum.txt
+cd ${BINARIES_DIR}/
+bsdtar -a -cf ${BINARIES_DIR}/${name}.tar.gz update -C update
