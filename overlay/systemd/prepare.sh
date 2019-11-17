26376463241421d875c30752561993e49853b9c7
Alexey <skif@skif-web.ru>
2019-11-17 13:37:13 +0300

    prepare.sh : miltiple changes
    Restore all settings from settings.txt, not from /data/ storage
    remove restore database from dump (not needed yet)
    current ip in issue now working in loop

--- overlay/systemd/prepare.sh
+++ overlay/systemd/prepare.sh
@@ -1,23 +1,6 @@
 #!/bin/bash --login
-
-#########################################################
-#
-# zabbix settings restore
-#
-#########################################################
-
-# If any zabbix config exist on data-partition, then use them
-if [ -f /data/zabbix_agentd.conf ]; then
-    cp /data/zabbix_agentd.conf /etc/zabbix_agentd.conf
-fi
-
-if [ -f /data/zabbix_server.conf ]; then
-    cp /data/zabbix_server.conf /etc/zabbix_server.conf
-fi
-
-if [ -f /data/zabbix.conf.php ]; then
-    cp /data/zabbix.conf.php /var/www/conf/zabbix.conf.php
-fi
+source /etc/profile.d/monitorVariables.sh 
+source $MY_LIB
 
 #########################################################
 #
@@ -34,23 +17,18 @@ chown zabbix:zabbix /run/zabbix
 #
 #########################################################
 
-# permissions
-psql --user postgres -c "CREATE USER zabbix WITH PASSWORD 'zabbix';" && \
-    psql --user postgres -c "CREATE DATABASE zabbix ENCODING 'Unicode' TEMPLATE template0 OWNER zabbix;" && \
-        psql --user postgres -c "GRANT ALL PRIVILEGES ON DATABASE zabbix TO zabbix;" 
+# # permissions
+zabbix_db_exist=`psql --user postgres -c "select datname from pg_database;"|grep zabbix`
+if [ "x$zabbix_db_exist" == "x" ]; then
 
-# Try to find any dump
-sqlDump=`ls -lt /data/fullDump*.sql 2>/dev/null|head -n1|awk '{print $NF}'`
-
-# If dump exist, then restore it
-# else use default files
-if [ "x$sqlDump" != "x" ]; then
-    psql -U zabbix -d zabbix -f $sqlDump
-else
+    user_zabbix_exist=`psql --user postgres -c "\du"|grep zabbix`
+    if [ "x$user_zabbix_exist" == "x" ];then
+        psql --user postgres -c "CREATE USER zabbix WITH PASSWORD 'zabbix';"
+    fi
+    
+    f_create_db
     psql -U zabbix -d zabbix -f /usr/zabbix/postgresql_schema/schema.sql
-    # sleep 0.5
     psql -U zabbix -d zabbix -f /usr/zabbix/postgresql_schema/images.sql
-    # sleep 0.5
     psql -U zabbix -d zabbix -f /usr/zabbix/postgresql_schema/data.sql
 fi
 
@@ -68,7 +46,10 @@ timedatectl list-timezones &> /var/www/manage/timezone.list
 #
 #########################################################
 
-current_ip=`ifconfig eth0 |grep 'inet addr'| cut -d":" -f2|awk '{print $1}'`
+current_ip=''
+while [ "x$current_ip" == "x" ]; do
+    current_ip=`ifconfig eth0 |grep 'inet addr'| cut -d":" -f2|awk '{print $1}'`
+done
 
 echo "current ip $current_ip" > /etc/issue
 echo "Ready to work" >> /etc/issue
