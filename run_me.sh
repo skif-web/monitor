7a42323f29ead37e158af8738eeb8bf2a9d0af99
Alexey <skif@skif-web.ru>
2020-01-16 17:34:51 +0300

    run_me.sh: make silent; add ramdisk option; fix remove file list

--- run_me.sh
+++ run_me.sh
@@ -1,36 +1,67 @@
 #!/bin/bash
+
+############################################################################
+#
+#   name: prepare buildroot for monitor-linux
+#   desctiption: prepare buildroot for selected board
+#   params:
+#       first position param is optional. If exist, prepare for board number = $1
+#       else show board list and need promt from user
+#   results placed in $GIT_DIRECTORY/buildroot-{version}
+#
+############################################################################
+
 clear
+
 # set -e
+
+# build in ramdisk
+USE_RAMDISK=y
+RAMDISK_SIZE=16G
+
 if [ "x$1" != "x" ]; then
 	board_num=$1
 fi
 
-
-exclude_files='overlay _config.yml ramdisk README.md run_me.sh .git buildroot-20*tar.gz \. '
+exclude_files='overlay _config.yml ramdisk LICENSE .gitignore README.md run_me.sh .git buildroot-20*tar.gz \. '
 find_cmd=""
 for exclude in $exclude_files
 do
 	find_cmd="! -iname $exclude $find_cmd"
 done 
 
-
 w_dir=$(readlink -f "$0")
 w_dir=$(dirname "$w_dir")
 ram_dir="$w_dir/ramdisk"
 overlay_dir="$w_dir/overlay"
 cd $w_dir
-buildroot_arch_name=`find . -maxdepth 1 -iname buildroot-20\*tar.gz `
+buildroot_arch_name=`find $w_dir/ -maxdepth 1 -iname buildroot-20\*tar.gz `
+buildroot_name=`basename $buildroot_arch_name .tar.gz`
+
 patch_dir=$overlay_dir/buildroot_patches
 
-echo patch_dir=$patch_dir
-echo w_dir=$w_dir
-echo overlay_dir=$overlay_dir
-echo b_n=$buildroot_arch_name
+# echo patch_dir=$patch_dir
+# echo w_dir=$w_dir
+# echo overlay_dir=$overlay_dir
+# echo buildroot_arch_name=$buildroot_arch_name
+# echo buildroot_name=$buildroot_name
+
+
+mount | grep -q "$w_dir/$buildroot_name"
+if [ $? -eq 0 ]; then
+    sudo umount -l $w_dir/$buildroot_name
+fi
 
 find  $w_dir/.  -maxdepth 1  $find_cmd| xargs rm -rf
 
+if [ "x$USE_RAMDISK" == "xy" ]; then
+    mkdir -p $w_dir/$buildroot_name
+    sudo mount -t tmpfs -o size=$RAMDISK_SIZE tmpfs $w_dir/$buildroot_name
+    sudo chown $USER $w_dir/$buildroot_name
+fi
+
+cd $w_dir
 tar xpf $buildroot_arch_name 
-buildroot_name=`basename $buildroot_arch_name .tar.gz`
 
 cd $buildroot_name
 defconfigs_array=()
@@ -38,7 +69,7 @@ for i in `find $overlay_dir/configs/ -type f -iname *_defconfig |awk -F\/ '{prin
 	defconfigs_array+=("$i")
 done
 
-echo size=${#defconfigs_array[@]}
+# echo size=${#defconfigs_array[@]}
 
 if [ ${#defconfigs_array[@]} -gt 0 ]; then
     index=0
