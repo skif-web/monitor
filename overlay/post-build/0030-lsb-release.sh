8047403e80265a2cdfa7725d8bc76a8038a42d11
Alexey <skif@skif-web.ru>
2020-01-16 17:36:40 +0300

    post-build: generate normal lsbrelease

--- overlay/post-build/0030-lsb-release.sh
+++ overlay/post-build/0030-lsb-release.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+############################################################################
+#
+#   description: create version-files in /etc/
+#   params: not avaible
+#
+############################################################################
+
 
 # /etc/monitor-release
 name=`grep BR2_TARGET_GENERIC_HOSTNAME .config|awk -F= '{print $2}'|tr -d \"`
 echo $name > ${TARGET_DIR}/etc/monitor-release
-# TODO
-# May be add /etc/os-release...may be...later...
+
+DISTRIB_ID=`echo $name|awk -F\- '{print $1}'`
+DISTRIB_RELEASE=`echo $name|awk -F\- '{print $2}'`
+
+echo "DISTRIB_ID=$DISTRIB_ID" >${TARGET_DIR}/etc/lsb_release
+echo "DISTRIB_RELEASE=$DISTRIB_RELEASE" >> ${TARGET_DIR}/etc/lsb_release
+echo "DISTRIB_DESCRIPTION=$DISTRIB_ID $DISTRIB_RELEASE" >> ${TARGET_DIR}/etc/lsb_release
