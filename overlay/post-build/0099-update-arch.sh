9d086ce0239a5885a41846847a799a7b91c97f4d
Alexey <skif@skif-web.ru>
2020-01-16 17:41:54 +0300

    post-build/autoupdate: fix kernel filename

--- overlay/post-build/0099-update-arch.sh
+++ overlay/post-build/0099-update-arch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+##################################################
+#
+# description: create update-arch
+# params: not avaible
+#
+##################################################
+
 update_dir="${BINARIES_DIR}/update"
 
 name=`grep BR2_TARGET_GENERIC_HOSTNAME .config|awk -F= '{print $2}'|tr -d \"`
@@ -9,7 +17,7 @@ rm -rf ${BINARIES_DIR}/${name}.tar.gz
 
 mkdir -p ${BINARIES_DIR}/update
 
-cp ${BINARIES_DIR}/bzImage ${update_dir}
+cp ${BINARIES_DIR}/*zImage ${update_dir}
 cp ${TARGET_DIR}/usr/bin/update.sh ${update_dir}/
 cd ${update_dir}/
 sha256sum * > checksum.txt
