e0d9ee41af1ebf27756ad726ead89e6fa56e4ca5
Alexey <skif@skif-web.ru>
2020-01-16 17:43:20 +0300

    rodos.sh: add description header

--- overlay/systemd/rodos.sh
+++ overlay/systemd/rodos.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+##################################################
+#
+# description: get temp from RODOS5_6 usb thermometer
+# params: not avaible
+#
+##################################################
+
 RODOS5_6 -r -a|tail -n 1|awk -F= '{print $NF}'|awk -F. '{print $1}'
