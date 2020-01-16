89fe081a08339908147a0e791ea09046fe486a65
Alexey <skif@skif-web.ru>
2020-01-16 17:41:01 +0300

    post-build: add description header

--- overlay/post-build.sh
+++ overlay/post-build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+##################################################
+#
+# description: run scripts on post-build stage
+# params: not avaible
+#
+##################################################
+
 set -x
 
 for file in ${BR2_EXTERNAL_monitorOverlay_PATH}/post-build/*
--- overlay/post-build/0020-sudo-for-web.sh
+++ overlay/post-build/0020-sudo-for-web.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+############################################################################
+#
+#   description: create sudoers for web-interface
+#   params: not avaible
+#
+############################################################################
+
 mkdir -p ${TARGET_DIR}/etc/sudoers.d
 echo "www-data ALL=(ALL) NOPASSWD: /usr/bin/libWebInterface.sh" > ${TARGET_DIR}/etc/sudoers.d/web_interface
